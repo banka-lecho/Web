f834a2faea9259730f5ffd4b042088d27560620c
Shpileva_Anastasiya <shpilyova.nastya@mail.ru>
2022-12-09 16:30:07 +0300

    web

---
+++ lesson10/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <div> Written posts: {{postsNumber}}</div>
+        <div> Users: {{usersNumber}}</div>
+        <a href="/">Codeforces</a> &copy; 2021 by Mike Mirzayanov
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+    props: ["usersNumber", "postsNumber"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ lesson11/backend/src/main/java/ru/itmo/wp/controller/UserController.java
@@ -0,0 +1,43 @@
+package ru.itmo.wp.controller;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.itmo.wp.domain.User;
+import ru.itmo.wp.exception.ValidationException;
+import ru.itmo.wp.form.RegisterInfo;
+import ru.itmo.wp.service.JwtService;
+import ru.itmo.wp.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/1")
+public class UserController {
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    public UserController(JwtService jwtService, UserService userService) {
+        this.jwtService = jwtService;
+        this.userService = userService;
+    }
+
+    @GetMapping("users/auth")
+    public User findUserByJwt(@RequestParam String jwt) {
+        return jwtService.find(jwt);
+    }
+
+    @GetMapping("users")
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @PostMapping("users")
+    public User register(@RequestBody @Valid RegisterInfo registerInfo, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException(bindingResult);
+        }
+
+        return userService.create(registerInfo);
+    }
+}
